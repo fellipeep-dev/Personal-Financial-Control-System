2726317ea680420b0dd75fb0a3b059cbff62184e
Fellipe Esteves Pereira <fellipe.esteves@vvssistemas.local>
2025-09-22 16:52:41 -0300

    feat: add new validations in user dtos

--- BackEnd/Application/Dtos/User/CreateUserDto.cs
+++ BackEnd/Application/Dtos/User/CreateUserDto.cs
@@ -1,15 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.Dtos.User
 {
     public class CreateUserDto
     {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "CPF is required")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "CPF must be in the format 000.000.000-00")]
         public string CPF { get; set; }
 
+        [Required(ErrorMessage = "BirthDate is required")]
         public DateOnly BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
 }
--- BackEnd/Application/Dtos/User/UpdateUserDto.cs
+++ BackEnd/Application/Dtos/User/UpdateUserDto.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.Dtos.User
 {
     public class UpdateUserDto
     {
         public Guid Id { get; set; }
 
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters")]
         public string? Name { get; set; }
 
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string? Email { get; set; }
 
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "CPF must be in the format 000.000.000-00")]
         public string? CPF { get; set; }
 
         public DateOnly? BirthDate { get; set; }
