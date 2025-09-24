b759161fbef1eeef9e54759a3669ef898f6341ba
Fellipe Esteves Pereira <fellipe.esteves@vvssistemas.local>
2025-09-24 17:32:59 -0300

    feat: add create and update category dots

---
+++ BackEnd/Application/Dtos/Category/CreateCategoryDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Category;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos.Category
+{
+    public class CreateCategoryDto
+    {
+        [Required(ErrorMessage = "UserId is required")]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "TotalInCents is required")]
+        public int TotalInCents { get; set; }
+
+        [Required(ErrorMessage = "Type is required")]
+        public CategoryType Type { get; set; }
+    }
+}
---
+++ BackEnd/Application/Dtos/Category/UpdateCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Category;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos.Category
+{
+    public class UpdateCategoryDto
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? TotalInCents { get; set; }
+
+        public CategoryType? Type { get; set; }
+    }
+}
