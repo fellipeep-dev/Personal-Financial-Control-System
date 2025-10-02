2d4cfbf1121fb6528be1ca87b71f9f1ea96b6d0c
Fellipe Esteves Pereira <fellipe.esteves@vvssistemas.local>
2025-10-02 17:38:37 -0300

    feat: add a generic createUseCase

---
+++ BackEnd/Application/Abstractions/UseCases/CreateUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Interfaces;
+
+namespace Application.Abstractions.UseCases
+{
+    public class CreateUseCase<TEntity, TCreateDto>(
+        IRepository<TEntity> repository
+    ) : ICreateUseCase<TCreateDto> where TEntity : class, new()
+    {
+        private readonly IRepository<TEntity> _repository = repository;
+
+        public async Task ExecuteAsync(TCreateDto createDto)
+        {
+            var entity = (TEntity)Activator.CreateInstance(typeof(TEntity), createDto)!;
+
+            await _repository.AddAsync(entity);
+        }
+    }
+}
