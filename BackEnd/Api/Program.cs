5351a1c56861790567fc6d60fc2063a55c5fadbe
Fellipe Esteves Pereira <fellipe.esteves@vvssistemas.local>
2025-10-03 14:18:18 -0300

    feat: add swagger

--- BackEnd/Api/Program.cs
+++ BackEnd/Api/Program.cs
@@ -2,6 +2,7 @@ using Application;
 using Infrastructure;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,33 @@ builder.Services.AddInfrastructureModule();
 builder.Services.AddApplicationModule();
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Person Financy Api",
+        Description = "",
+        Contact = new OpenApiContact
+        {
+            Name = "Fellipe",
+            Email = "fellipeep2005@gmail.com"
+        }
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api V1");
+        c.RoutePrefix = "swagger";
+    });
+}
 
 app.MapControllers();
 
