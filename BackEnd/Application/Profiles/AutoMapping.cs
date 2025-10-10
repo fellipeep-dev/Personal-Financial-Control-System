ba32875c6df43e008623100576b71d7b2aef59db
Fellipe Esteves Pereira <fellipe.esteves@vvssistemas.local>
2025-10-10 17:49:28 -0300

    feat: add User mapping in AutoMapping

--- BackEnd/Application/Profiles/AutoMapping.cs
+++ BackEnd/Application/Profiles/AutoMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Dtos.Category;
 using Application.Dtos.Transaction;
+using Application.Dtos.User;
 using AutoMapper;
 using Domain.Entities;
 
@@ -35,6 +36,30 @@ namespace Application.Profiles
                 .ForMember(
                     dest => dest.Type,
                     opt => opt.Condition(src => src.Type != 0 && src.Type.HasValue)
+                );
+
+            CreateMap<CreateUserDto, UserEntity>();
+            CreateMap<UpdateUserDto, UserEntity>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.Condition(src => !string.IsNullOrWhiteSpace(src.Name))
+                )
+                .ForMember(
+                    dest => dest.Email,
+                    opt => opt.Condition(src => !string.IsNullOrWhiteSpace(src.Email))
+                )
+                .ForMember(
+                    dest => dest.Password,
+                    opt => opt.Condition(src => !string.IsNullOrWhiteSpace(src.Password))
+                )
+                .ForMember(
+                    dest => dest.Cpf,
+                    opt => opt.Condition(src => !string.IsNullOrWhiteSpace(src.Cpf))
+                )
+                .ForMember(
+                    dest => dest.BirthDate,
+                    opt => opt.Condition(src => src.BirthDate.HasValue)
                 );
         }
     }
